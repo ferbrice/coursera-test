a01fddfca61100e4a1435aedcc5566909561bb3c
Fernando Briceño <ferbrice@hotmail.com>
2022-01-27 22:34:14 -0500

    terminando lo solicitado

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,16 +1,22 @@
 (function () {
-'use strict';
+  'use strict';
 
-angular.module('LunchCheck', [])
-.controller('LunchCheckController', LunchCheckController);
+  angular.module('LunchCheck', [])
+  .controller('LunchCheckController', LunchCheckController);
 
-LunchCheckController.$inject = ['$scope', '$filter'];
-function LunchCheckController($scope, $filter) {
-  $scope.message = "";
+  LunchCheckController.$inject = ['$scope','$filter'];
 
-  $scope.checkLunch = function () {
-    $scope.message = "Trabajando aún";
+  function LunchCheckController($scope,$filter) {
+    $scope.message = "";
+    $scope.listMenu = ""; 
+	$scope.listMenuItems=[];
+    $scope.checkLunch = function () {
+      $scope.listMenuItems = $scope.listMenu.split(",");
+      $scope.listMenu.length==0 ? 
+		$scope.message="Please enter data first":
+		$scope.listMenuItems.length>3 ?
+			$scope.message="Too much!":
+			$scope.message="Enjoy!";
+	};
   };
-}
-
 })();
